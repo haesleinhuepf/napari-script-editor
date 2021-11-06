ff62ec611ee407428c5d8968ba42d447b1e75540
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-06 10:40:21 +0100

    fill the Tools > Scripts menu

--- src/napari_script_editor/__init__.py
+++ src/napari_script_editor/__init__.py
@@ -6,3 +6,6 @@ __version__ = "0.0.1"
 
 from ._dock_widget import napari_experimental_provide_dock_widget
 
+from ._scripts_directory import _search_scripts
+
+_search_scripts()
--- src/napari_script_editor/_scripts_directory.py
+++ src/napari_script_editor/_scripts_directory.py
@@ -1,6 +1,15 @@
+import os
+from napari_tools_menu import register_action
+
+CODE_TEMPLATE = """import napari
+if 'viewer' not in globals():
+    viewer = napari.Viewer()
+print("Hello world, napari has", len(viewer.layers), "layers")
+"""
+
 
 def _init_scripts_directory():
-    import os
+
     home_dir = os.path.expanduser('~')
     sub_dir = "/.napari-scripts/"
 
@@ -14,10 +23,39 @@ def _init_scripts_directory():
 
     if not os.path.isdir(scripts_dir + _new_template_filename()):
         with open(scripts_dir + _new_template_filename(), 'w') as f:
-            f.write('print("Hello world")\n')
+            f.write(CODE_TEMPLATE)
 
     return scripts_dir
 
 def _new_template_filename():
     return '.new_template.py'
 
+def _search_scripts():
+    print("Searching for menus")
+    file_list = os.listdir(_init_scripts_directory())
+    print("FL", file_list)
+    files = [file for file in file_list if file.endswith(".py") and not file.startswith(".")]
+    print("FL2", files)
+
+    for i in files:
+        print(i)
+        _register_menu(_init_scripts_directory() + i)
+
+def _register_menu(filename):
+
+    with open(filename) as f:
+        lines = f.readlines()
+    code = "\n".join(lines)
+
+    menu = filename.replace("\\", "/").split("/")[-1].replace("_", " ").replace(".py", "")
+
+    def callback(viewer):
+        _exec_code(code, viewer)
+
+    print("REG", menu)
+
+    register_action(callback, "Scripts > " + menu)
+
+def _exec_code(code, viewer):
+    globs = {"viewer": viewer}
+    exec(code, globs)
